0d7ec0bd84b13871791fffaaefecac8ed1c79238
Madhavi T <Madhavi.dusari15@gmail.com>
2025-05-04 11:21:09 +0530

    Started Java Programming

---
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Madhavi");
+    }
+
+
+
+}
