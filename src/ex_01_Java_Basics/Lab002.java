7c795de4890fc21e3ccfa97fd744e192ec334d15
Madhavi T <Madhavi.dusari15@gmail.com>
2025-05-10 11:11:26 +0530

    Started Java Programming

---
+++ src/ex_01_Java_Basics/Lab002.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("This is a Statement!");
+        System.out.println("2+2");
+        System.out.println(2+2);
+    }
+}
